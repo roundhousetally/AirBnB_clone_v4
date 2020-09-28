5eef07984eabd5d5205a5157db43b20098491155
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-28 05:44:24 +0000

    task 4 complete

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -32,17 +32,17 @@ $(document).ready(function () {
     data: '{}',
     success: function (result) {
       for (const i in result) {
-        const first = '<article>\n<h2><div class="title_box">' + result[i].name + '</h2>   <div class="price_by_night">' + result[i].price_by_night + '</div></div>\n   <div class="information" role="list">\n      <div class="max_guest" title="Number of Guests"><h3>' + result[i].max_guest + 'Guest';
+        const first = '<article>\n<div class="title_box"><h2>' + result[i].name + '</h2>   <div class="price_by_night">' + result[i].price_by_night + '</div></div>\n   <div class="information" role="list">\n      <div class="max_guest" title="Number of Guests">' + result[i].max_guest + ' Guest';
         let second = '';
         if (result[i].max_guest !== 1) {
           second = second + 's';
         }
-        second = second + '</h3></div>\n      <div class="number_rooms">' + result[i].number_rooms + 'Bedroom';
+        second = second + '</div>\n      <div class="number_rooms">' + result[i].number_rooms + ' Bedroom';
         let third = '';
         if (result[i].number_rooms !== 1) {
           third = third + 's';
         }
-        third = third + '</div>\n<div class="number_bathrooms">' + result[i].number_bathrooms + 'Bathroom';
+        third = third + '</div>\n<div class="number_bathrooms">' + result[i].number_bathrooms + ' Bathroom';
         let fourth = '';
         if (result[i].number_bathrooms !== 1) {
           fourth = fourth + 's';
