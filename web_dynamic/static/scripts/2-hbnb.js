4879057c965a3a25602c548d1b0d6f8b8d9bbe1d
Brett Davis <brett.p.davis101@gmail.com>
2020-09-28 01:46:11 +0000

    Updates to 4: adds template for places search, needs testing

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -2,29 +2,27 @@ const ch = [];
 
 $(document).ready(function () {
   $('li input').click(function () {
-    if($(this).is(':checked')) {
-      let tmp = $(this).attr('data-name');
+    if ($(this).is(':checked')) {
       ch.push($(this).attr('data-name'));
-      $('.amenities h4').text(ch.join(", "));
-    }
-    else {
+      $('.amenities h4').text(ch.join(', '));
+    } else {
       for (let i = 0; i < ch.length; i++) {
         if (ch[i] === $(this).attr('data-name')) {
           ch.splice(i, 1);
         }
       }
       if (ch !== undefined && ch.length > 0) {
-        $('.amenities h4').text(ch.join(", "));
-      }
-      else {
+        $('.amenities h4').text(ch.join(', '));
+      } else {
         $('.amenities h4').text('\xa0');
       }
     }
   });
-  $.getJSON("http://localhost:5001/api/v1/status/", function(data){
-    if (data.status === 'OK'){
+  $.getJSON('http://localhost:5001/api/v1/status/', function (data) {
+    $('.locations h4').text(data);
+    if (data.status === 'OK') {
       $('div#api_status').addClass('available');
-    }else{
+    } else {
       $('div#api_status').removeClass('available');
     }
   });
