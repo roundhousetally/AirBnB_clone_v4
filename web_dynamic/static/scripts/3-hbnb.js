15c2b90d37ef23c926d5697effb7297a0cf84d38
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-29 00:42:35 +0000

    added the dollar sign back into price by night

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
     data: '{}',
     success: function (result) {
       for (const i in result) {
-        const first = '<article>\n<div class="title_box"><h2>' + result[i].name + '</h2>   <div class="price_by_night">' + result[i].price_by_night + '</div></div>\n   <div class="information" role="list">\n      <div class="max_guest" title="Number of Guests">' + result[i].max_guest + ' Guest';
+        const first = '<article>\n<div class="title_box"><h2>' + result[i].name + '</h2>   <div class="price_by_night">' + '$' + result[i].price_by_night + '</div></div>\n   <div class="information" role="list">\n      <div class="max_guest" title="Number of Guests">' + result[i].max_guest + ' Guest';
         let second = '';
         if (result[i].max_guest !== 1) {
           second = second + 's';
